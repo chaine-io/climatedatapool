363b63c85ff125c4b1bf4168c3926074874d4d2c
Data Beetle <abhimanyu.sarvagyam@gmail.com>
2020-07-31 13:13:16 +0800

    Added comments to getLists.js

--- getLists.js
+++ getLists.js
@@ -1,4 +1,8 @@
+// Impprt Fleek Storage SDK
+
 const fleekStorage = require('@fleekhq/fleek-storage-js')
+
+// Function to list files
 
 async function fileList() {
 
@@ -16,6 +20,8 @@ getOptions: [
 console.log('files', files)
 }
 
+// Fuction to list buckets
+
 async function bucketList() {
 
 const buckets = await fleekStorage.listBuckets({
@@ -27,9 +33,13 @@ console.log('buckets', buckets)
 
 }
 
+// Run the functions
+
 async function run() {
 await bucketList()
 await fileList()
 }
+
+// Error handling
 
 run().then(() => console.log('done')).catch(console.error)
